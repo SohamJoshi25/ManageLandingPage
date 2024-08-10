d5d6739ff0635dbc32e4a9beec07b784b1723907
Soham Joshi <sohamjoshichinchwad@gmail.com>
2024-08-10 21:11:02 +0530

    Background Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,16 @@ const links = ["Price","Product","About Us","Careers","Community"]
 
 const Navbar = () =>{
     return (
-        <nav className=' flex flex-row justify-between md:justify-around mx-8 px-8 p-8  items-center'>
-            <img src="./logo.svg" alt="" />
-            <div className='hidden md:flex flex-rows md:text-sm lg:text-lg space-x-8'>
+        <nav className='relative flex flex-row justify-between md:justify-around mx-8 px-8 p-8  z-10 items-center'>
+            <img className='absolute z-0' src="./bg-tablet-pattern.svg" alt="" />
+            <img className=' z-10' src="./logo.svg" alt="" />
+            <div className='hidden md:flex flex-rows md:text-sm lg:text-lg space-x-8  z-10'>
                 {links.map(element => {
                     return <a href="#">{element}</a>
                 })}
             </div>
-            <button className='bg-orange-500 px-6 py-2 md:text-sm lg:text-lg  text-white rounded-full shadow-md hover:bg-orange-600'>
-                Contact Us
+            <button className='bg-orange-500 px-6 py-2 md:text-sm lg:text-lg  text-white rounded-full shadow-md hover:bg-orange-600  z-10'>
+                Contact Us.
             </button>
         </nav>
     )
